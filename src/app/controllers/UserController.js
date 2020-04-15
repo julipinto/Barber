1d41a658c5cddd9753df752267a41ba1caa71416
Juliapp <aragaopintojuli@gmail.com>
2020-04-15 09:13:05 -0300

    autenticação jwt implementada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -11,6 +11,22 @@ class UserController {
         const { id, name, email, provider } = await User.create(req.body);
         return res.json({ id, name, email, provider });
     }
+
+    async update(req, res) {
+        const { email, oldPassword } = req.body;
+        const user = await User.findByPk(req.userId);
+        oldPassword.ok();
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email },
+            });
+            if (userExists) {
+                return res.status(400).json({ error: 'User already exists' });
+            }
+        }
+        return res.send();
+    }
 }
 
 export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: '5021f3dc6cc7420c4681d1b6373766a3',
+    expiresIn: '7D',
+};
