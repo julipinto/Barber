2fcebe3d4ec9463695d630108b014fa8122afa09
Juliapp <aragaopintojuli@gmail.com>
2020-04-28 13:18:01 -0300

    criando a tabela file

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,10 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
-import User from '../app/models/User';
 
-const models = [User];
+import User from '../app/models/User';
+import File from '../app/models/File';
+
+const models = [User, File];
 
 class Database {
     constructor() {
